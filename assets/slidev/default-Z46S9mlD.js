036b35059733e3a6e9258e76476ac9204c7452ac
gidragir <gidragir@users.noreply.github.com>
2026-02-03 14:39:28 +0000

    deploy: c4c0caefa881be92203046b8ad7a801494825e78

--- assets/slidev/default-Z46S9mlD.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-C54SBDPq.js";import{_ as r}from"../index-BC7g9U9z.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
